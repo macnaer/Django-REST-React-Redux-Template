f30eaf433a872c6c6cb7a7cca4f360b911d441bc
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2021-02-03 19:24:15 +0200

    Fix issue with css

--- app/contact_list/admin.py
+++ app/contact_list/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactListModel
+
+
+class ContactListAdmin(admin.ModelAdmin):
+    list_display = ('id', "name", "surname", "role",
+                    "avatar", "status", "email", "gender")
+    list_filter = ("name",)
+    list_filter = ("surname",)
+    list_per_page = 300
+
+
+admin.site.register(ContactListModel, ContactListAdmin)
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,14 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+      {​​
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      }​​,
     ]
   }
 }
